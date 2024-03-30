15c1434703bb5577ed405191c0af3e6194f8b44f
xxmayankxx <mayank@999designs.in>
2024-03-30 07:42:00 -0400

    adding rest of the files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Montserrat } from 'next/font/google';
+export const montserrat = Montserrat({ weight: '400', subsets: ['latin'] });
